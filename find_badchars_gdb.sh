707f63bfb2c6aeba5a8a4a7165a8ad1e8b3c81f1
kp <contact@kernel-panik.me>
2021-07-15 12:17:03 +0200

    add gdb

---
+++ find_badchars_gdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# x/200xw $esp
+
+file_input="$1"
+file_badchars="$2"
+
+the_input="$(cat "$1" | awk -F"\t" '{print $2$3$4$5}' | sed 's/0x/\n/g' | sed '/^$/d' | sed 's/../&:/g;s/:$//' | awk -F":" '{print $4$3$2$1}' | tr -d "\n" | sed 's/../&:/g;s/:$//' | tr '[:lower:]' '[:upper:]')"
+the_badchar="$(cat "$2" | tr -d "\\" | tr -d "x" | sed 's/../&:/g;s/:$//' | tr '[:lower:]' '[:upper:]')"
+
+echo "$the_input" > /tmp/theinput.txt
+echo "$the_badchar" > /tmp/thebadchar.txt
+
+vimdiff /tmp/theinput.txt /tmp/thebadchar.txt
+
